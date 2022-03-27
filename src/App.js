151f196a6626c48b614b44307584cce8fc649835
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-03-27 12:51:43 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo2.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+      Hello
+    </div>
+  );
+}
+
+export default App;
