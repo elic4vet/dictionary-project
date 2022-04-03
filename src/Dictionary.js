1ad94c30c3c32c2e7df1c4c1adc300839af0608a
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-04-03 19:06:32 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,10 +5,9 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState({});
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
