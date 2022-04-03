0e60387b5dbf85665a8cb1eaa5d93a7f0807d3df
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-04-03 12:54:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from "./logo2.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
-      Hello
+      <main>
+        <Dictionary />
+      </main>
+
+      <footer className="App-footer">Coded by Elisabeth Erkekoglou </footer>
     </div>
   );
 }
-
-export default App;
