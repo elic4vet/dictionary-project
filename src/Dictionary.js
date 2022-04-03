daa51733741084203e9d04d5d0e73bc80dc2e86b
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-04-03 13:35:42 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [keyword, SetKeyword] = useState("");
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState({});
 
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
-    SetKeyword(event.target.value);
+    setKeyword(event.target.value);
   }
 
   return (
@@ -24,6 +27,8 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="Search" onChange={handleKeywordChange} />
       </form>
+
+      <Results results={results} />
     </div>
   );
 }
