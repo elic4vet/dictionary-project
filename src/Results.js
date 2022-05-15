5f988f2fcf4ab77aa9047baa7cc4a54c318321ac
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-05-15 16:47:19 +0200

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Result(props) {
   console.log(props.results);
@@ -8,6 +9,13 @@ export default function Result(props) {
     return (
       <div className="Results">
         <h2> {props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
