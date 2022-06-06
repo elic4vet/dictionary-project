c1e177519a4802b02b3cf6fbcacaeea5b56bcb23
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-06-06 14:14:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <a href="app.js">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />{" "}
+        </a>
       </header>
       <main>
-        <Dictionary defaultKeyword="sunset" />
+        <Dictionary
+          defaultKeyword="sunset
+      "
+        />
       </main>
 
-      <footer className="App-footer">Coded by Elisabeth Erkekoglou </footer>
+      <footer className="App-footer">
+        ✌️ This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/elisavet-erkekoglou-80514b107/">
+          Elisabeth
+        </a>
+        👩‍💻 and it is{" "}
+        <a href="https://github.com/elic4vet/dictionary-project">
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
