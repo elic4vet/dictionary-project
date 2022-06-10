81f5d0773bbe24a6a427ce642fa7c22199a34a4a
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-06-10 21:10:24 +0200

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -12,13 +12,18 @@ export default function Result(props) {
       <div className="Results">
         <section>
           <h2> {props.results.word}</h2>
-          {props.results.phonetics.map(function (phonetic, index) {
-            return (
-              <div key={index}>
-                <Phonetic phonetic={phonetic} />
-              </div>
-            );
-          })}
+
+          {props.results.phonetics
+            .filter(function (phonetic) {
+              return phonetic.audio;
+            })
+            .map(function (phonetic, index) {
+              return (
+                <div key={index}>
+                  <Phonetics phonetic={phonetic} />
+                </div>
+              );
+            })}
         </section>
 
         {props.results.meanings.map(function (meaning, index) {
